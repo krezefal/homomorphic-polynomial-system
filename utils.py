dc458c76977b70dcefbfb732cb9c993d2cbf1a13
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-05-27 03:09:48 +0300

    fix bug when creating obfuscating polynomial; reorganize the code + switch to using numpy poly1d (everywhere)

--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
 import secrets
-
 import numpy as np
-from numpy.polynomial import Polynomial
+
+from vars import *
 
 
-def generate_obfuscating_multiplier(even_degree, coef_max_val: int) -> Polynomial:
+def generate_obfuscating_multiplier(even_degree: int) -> np.poly1d:
+
+    assert even_degree % 2 == 0, "Must be EVEN for the polynomial to have no rational roots"
+
     if even_degree != 0:
-        coef_at_passed_deg = secrets.randbelow(coef_max_val) + 1
-        coef_at_zero_deg = secrets.randbelow(coef_max_val) + 1
-        obfuscating_polynomial = Polynomial([coef_at_zero_deg, np.zeros(even_degree - 1), coef_at_passed_deg])
+        coef_at_passed_deg = secrets.randbelow(MAX_COEFFICIENT_VALUE) + 1
+        coef_at_zero_deg = secrets.randbelow(MAX_COEFFICIENT_VALUE) + 1
+        obfuscating_polynomial = np.poly1d([coef_at_passed_deg, *(np.zeros(even_degree - 1)), coef_at_zero_deg])
         return obfuscating_polynomial
     else:
-        coef_at_zero_deg = secrets.randbelow(coef_max_val) + 1
-        obfuscating_const = Polynomial(coef_at_zero_deg)
+        coef_at_zero_deg = secrets.randbelow(MAX_COEFFICIENT_VALUE) + 1
+        obfuscating_const = np.poly1d(coef_at_zero_deg)
         return obfuscating_const
