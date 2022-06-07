5e7b879d82c497e6a3b23c88d3366813fb55d53b
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-07 21:08:38 +0300

    add some useful funcs

--- abramov_system/utils.py
+++ abramov_system/utils.py
@@ -5,7 +5,6 @@ from .vars import *
 
 
 def generate_obfuscating_multiplier(even_degree: int) -> np.poly1d:
-
     assert even_degree % 2 == 0, "Must be EVEN for the polynomial to have no rational roots"
 
     if even_degree != 0:
@@ -17,3 +16,17 @@ def generate_obfuscating_multiplier(even_degree: int) -> np.poly1d:
         coef_at_zero_deg = secrets.randbelow(MAX_COEFFICIENT_VALUE) + 1
         obfuscating_const = np.poly1d(coef_at_zero_deg)
         return obfuscating_const
+
+
+def is_number(polynomial: np.poly1d) -> bool:
+    if polynomial.order == 0:
+        return True
+    else:
+        return False
+
+
+def is_zero(polynomial: np.poly1d) -> bool:
+    if polynomial.order == 0 and polynomial[0] == 0:
+        return True
+    else:
+        return False
