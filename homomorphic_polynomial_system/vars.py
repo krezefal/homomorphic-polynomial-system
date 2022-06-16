c69e9bffa56c33758b4b6a839f5f63f77a038fb7
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-17 00:16:19 +0300

    add some vars; reorganize file content for greater clarity

---
+++ homomorphic_polynomial_system/vars.py
@@ -0,0 +1,12 @@
+# encryption configuration #
+
+# inner
+MAX_COEFFICIENT_VALUE = 5
+
+# outer
+ROUND_TO_REAL_NUMBERS = 10
+ROUND_TO_INT = 1
+
+# recommended values for ~real cases
+MAX_POLYNOMIAL_DEGREE = 100000
+MIN_POLYNOMIAL_DEGREE = 10000
