d8f2883435819e6a904bc1e611f510d10d167771
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-17 00:21:59 +0300

    total update math tests according to new cryptosystem structure

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,13 +1,14 @@
 import sys
 import unittest
 import logging
+import numpy as np
 
-from abramov_system.keygen import generate_abramov_keypair
-from abramov_system.utils import is_number, is_zero
+from homomorphic_polynomial_system.keygen import generate_abramov_keypair
+from homomorphic_polynomial_system.vars import ROUND_TO_INT, ROUND_TO_REAL_NUMBERS
 
 
 class TestMath(unittest.TestCase):
-    reference_base = 9
+    reference_base = 7
     reference_degree = 8
 
     @classmethod
@@ -16,65 +17,116 @@ class TestMath(unittest.TestCase):
         cls.private_key, cls.public_key = \
             generate_abramov_keypair(cls.reference_base, cls.reference_degree)
 
-        cls.test_number1 = 197
-        cls.test_number2 = 27
+        cls.test_number1 = 56
+        cls.test_number2 = 112
+        cls.test_small_number1 = 2
+        cls.test_small_number2 = 3
 
         cls.encrypted_number1 = cls.public_key.encrypt(cls.test_number1)
         cls.encrypted_number2 = cls.public_key.encrypt(cls.test_number2)
+        cls.encrypted_small_number1 = cls.public_key.encrypt(cls.test_small_number1)
+        cls.encrypted_small_number2 = cls.public_key.encrypt(cls.test_small_number2)
 
     def test_addition(self):
         addition_result = self.test_number1 + self.test_number2
         enc_addition_result = self.encrypted_number1 + self.encrypted_number2
         dec_addition_result = self.private_key.decrypt(enc_addition_result)
+        rounded_dec_addition_result = np.round(dec_addition_result, ROUND_TO_INT)
 
         self.log.debug(f"\nAddition result: {addition_result}\n")
-        self.log.debug(f"\nDecrypted result: {dec_addition_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_addition_result}\n")
 
-        self.assertEqual(addition_result, dec_addition_result)
+        self.assertEqual(addition_result, rounded_dec_addition_result)
 
     def test_subtraction(self):
         subtraction_result = self.test_number1 - self.test_number2
         enc_subtraction_result = self.encrypted_number1 - self.encrypted_number2
         dec_subtraction_result = self.private_key.decrypt(enc_subtraction_result)
+        rounded_dec_subtraction_result = np.round(dec_subtraction_result, ROUND_TO_INT)
 
         self.log.debug(f"\nSubtraction result: {subtraction_result}\n")
-        self.log.debug(f"\nDecrypted result: {dec_subtraction_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_subtraction_result}\n")
 
-        self.assertEqual(subtraction_result, dec_subtraction_result)
+        self.assertEqual(subtraction_result, rounded_dec_subtraction_result)
 
     def test_multiplication(self):
         multiplication_result = self.test_number1 * self.test_number2
         enc_multiplication_result = self.encrypted_number1 * self.encrypted_number2
         dec_multiplication_result = self.private_key.decrypt(enc_multiplication_result)
+        rounded_dec_multiplication_result = np.round(dec_multiplication_result, ROUND_TO_INT)
 
         self.log.debug(f"\nMultiplication result: {multiplication_result}\n")
-        self.log.debug(f"\nDecrypted result: {dec_multiplication_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_multiplication_result}\n")
 
-        self.assertEqual(multiplication_result, dec_multiplication_result)
+        self.assertEqual(multiplication_result, rounded_dec_multiplication_result)
 
     def test_division(self):
-        if is_zero(self.encrypted_number2):
-            raise ZeroDivisionError()
-
         division_result = self.test_number1 / self.test_number2
-        enc_q, enc_r = self.encrypted_number1 / self.encrypted_number2
+        rounded_division_result = np.round(division_result, ROUND_TO_REAL_NUMBERS)
+        whole_part, remains, divider = self.encrypted_number1 / self.encrypted_number2
 
-        dec_q = self.private_key.decrypt(enc_q)
-        dec_r = self.private_key.decrypt(enc_r)
-        dec_num2 = self.private_key.decrypt(self.encrypted_number2)
+        dec_whole_part = self.private_key.decrypt(whole_part)
+        dec_remains = self.private_key.decrypt(remains)
+        dec_divider = self.private_key.decrypt(divider)
 
-        if is_number(self.encrypted_number1) and is_number(self.encrypted_number2):
-            self.log.debug(f"\nDivision result: {division_result}\n")
-            self.log.debug(f"\nDecrypted result: {dec_q}\n")
-            self.assertEqual(round(division_result, 10), round(dec_q, 10))  # ROUNDING!
-            return
+        dec_division_result = dec_whole_part + dec_remains / dec_divider
+        rounded_dec_division_result = np.round(dec_division_result, ROUND_TO_REAL_NUMBERS)
 
-        dec_division_result = dec_q + dec_r / dec_num2
+        self.log.debug(f"\nDivision result: {rounded_division_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_division_result}\n")
 
-        self.log.debug(f"\nDivision result: {division_result}\n")
-        self.log.debug(f"\nDecrypted result: {dec_division_result}\n")
+        self.assertEqual(rounded_division_result, rounded_dec_division_result)
 
-        self.assertEqual(round(division_result, 10), round(dec_division_result, 10))
+    def test_small_num_division(self):
+        division_result = self.test_small_number1 / self.test_small_number2
+        rounded_division_result = np.round(division_result, ROUND_TO_REAL_NUMBERS)
+        whole_part, remains, divider = self.encrypted_small_number1 / self.encrypted_small_number2
+
+        dec_whole_part = self.private_key.decrypt(whole_part)
+        dec_remains = self.private_key.decrypt(remains)
+        dec_divider = self.private_key.decrypt(divider)
+
+        dec_division_result = dec_whole_part + dec_remains / dec_divider
+        rounded_dec_division_result = np.round(dec_division_result, ROUND_TO_REAL_NUMBERS)
+
+        self.log.debug(f"\nDivision result: {rounded_division_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_division_result}\n")
+
+        self.assertEqual(rounded_division_result, rounded_dec_division_result)
+
+    def test_norm_divisible_small_divider(self):
+        division_result = self.test_number1 / self.test_small_number2
+        rounded_division_result = np.round(division_result, ROUND_TO_REAL_NUMBERS)
+        whole_part, remains, divider = self.encrypted_number1 / self.encrypted_small_number2
+
+        dec_whole_part = self.private_key.decrypt(whole_part)
+        dec_remains = self.private_key.decrypt(remains)
+        dec_divider = self.private_key.decrypt(divider)
+
+        dec_division_result = dec_whole_part + dec_remains / dec_divider
+        rounded_dec_division_result = np.round(dec_division_result, ROUND_TO_REAL_NUMBERS)
+
+        self.log.debug(f"\nDivision result: {rounded_division_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_division_result}\n")
+
+        self.assertEqual(rounded_division_result, rounded_dec_division_result)
+
+    def test_small_divisible_norm_divider(self):
+        division_result = self.test_small_number1 / self.test_number2
+        rounded_division_result = np.round(division_result, ROUND_TO_REAL_NUMBERS)
+        whole_part, remains, divider = self.encrypted_small_number1 / self.encrypted_number2
+
+        dec_whole_part = self.private_key.decrypt(whole_part)
+        dec_remains = self.private_key.decrypt(remains)
+        dec_divider = self.private_key.decrypt(divider)
+
+        dec_division_result = dec_whole_part + dec_remains / dec_divider
+        rounded_dec_division_result = np.round(dec_division_result, ROUND_TO_REAL_NUMBERS)
+
+        self.log.debug(f"\nDivision result: {rounded_division_result}\n")
+        self.log.debug(f"\nDecrypted result: {rounded_dec_division_result}\n")
+
+        self.assertEqual(rounded_division_result, rounded_dec_division_result)
 
 
 if __name__ == '__main__':
